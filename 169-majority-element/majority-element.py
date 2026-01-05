a68523681d5ca0a5726e2dcc750f3e5dac96f895
Sivaranjani S <161628477+sivaranjani-mspluto@users.noreply.github.com>
2026-01-06 00:30:23 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (92.51%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,14 +1,4 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hp={}
-        m=0
-        for i in nums:
-            if i not in hp:
-                hp[i]=1
-            else:
-                hp[i]+=1
-            m=max(m,hp[i])
-        for j in hp:
-            if hp[j]==m:
-                return j
-        return nums[0]
+        nums = sorted(nums)
+        return nums[len(nums)//2]
