483d8e2a4c795126ca62b72decc98734c8813c28
Sivaranjani S <161628477+sivaranjani-mspluto@users.noreply.github.com>
2026-01-09 10:34:30 +0530

    Time: 3 ms (42.45%) | Memory: 23.4 MB (8.31%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -4,7 +4,7 @@ class Solution:
         Do not return anything, modify s in-place instead.
         """
         left,right=0,len(s)-1
-        while left<right:
+        while left<=right:
             s[left],s[right]=s[right],s[left]
             left, right = left+1,right-1
 
