63d73b52ed90e582cc1fbbfb2a5c2348d279503c
Sivaranjani S <161628477+sivaranjani-mspluto@users.noreply.github.com>
2026-02-04 11:20:02 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (47.75%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,17 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        d={0:0,1:0,2:0}
+        #d={0:0,1:0,2:0}
+        a,b,c=0,0,0
         for i,ele in enumerate(nums):
             if ele==0:
-                d[0]+=1
+                a+=1
             elif ele==1:
-                d[1]+=1
+                b+=1
             else:
-                d[2]+=1
+                c+=1
         nums.clear()
-        for i in range(0,d[0]): nums.append(0)
-        for i in range(0,d[1]): nums.append(1)
-        for i in range(0,d[2]): nums.append(2)
+        for i in range(0,a): nums.append(0)
+        for i in range(0,b): nums.append(1)
+        for i in range(0,c): nums.append(2)
+
+        # k=0
+        # for i in range(0,d[0]): nums.
 
         
