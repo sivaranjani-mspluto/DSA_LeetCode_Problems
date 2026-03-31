a3750747294f9943ff66f45401289dcf822d2a42
Sivaranjani S <161628477+sivaranjani-mspluto@users.noreply.github.com>
2026-03-31 11:57:37 +0530

    Time: 134 ms (87.33%) | Memory: 25 MB (78.30%) - LeetSync

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        count=0
+        for i in s:
+            if i=="[":
+                count+=1
+            else:
+                if count>0:
+                    count-=1
+        return (count+1)//2
