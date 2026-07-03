731cbe78a754c0c49b7063f12bd4dfbd0b843807
Sivaranjani S <161628477+sivaranjani-mspluto@users.noreply.github.com>
2026-07-03 16:36:19 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (86.55%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -6,12 +6,29 @@
 #         self.right = right
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        def dfs_minpath(node):
-            if not node:
+        # def dfs_minpath(node):
+        #     if not node:
+        #         return 0
+        #     if not node.left:
+        #         return 1 + dfs_minpath(node.right)
+        #     if not node.right:
+        #         return 1 + + dfs_minpath(node.left)
+        #     return 1 + min(dfs_minpath(node.left),dfs_minpath(node.right))
+        # return dfs_minpath(root)
+
+        def bfs_minpath(root):
+            if not root:
                 return 0
-            if not node.left:
-                return 1 + dfs_minpath(node.right)
-            if not node.right:
-                return 1 + + dfs_minpath(node.left)
-            return 1 + min(dfs_minpath(node.left),dfs_minpath(node.right))
-        return dfs_minpath(root)
+            queue = deque([(root,1)])
+            level =1
+            while queue:
+                node,level = queue.popleft()
+                #if its a left node
+                if not node.left and not node.right:
+                    return level
+                if node.left:
+                    queue.append((node.left,level+1))
+                if node.right:
+                    queue.append((node.right,level+1))
+            return 0
+        return bfs_minpath(root)
