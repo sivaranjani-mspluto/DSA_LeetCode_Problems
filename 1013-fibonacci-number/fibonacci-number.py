cc092f833f5198a7fc4c48d387892621c617df6c
Sivaranjani S <161628477+sivaranjani-mspluto@users.noreply.github.com>
2026-07-08 10:29:17 +0530

    Time: 51 ms (48.27%) | Memory: 19.2 MB (17.91%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,11 +1,14 @@
+#third=0
 class Solution:
     def fib(self, n: int) -> int:
-        # not optimized 
-        def fibo(n):
-            if n==0:
-                return 0
-            if n==1:
-                return 1
-            return fibo(n-1)+fibo(n-2)
-        return fibo(n)
-        
+        third = 0
+        dp=[-1]*7
+        first=0
+        second=1
+        if n<=1:
+            return n
+        for i in range(2,n+1):
+            third=first+second
+            first=second
+            second=third
+        return third
